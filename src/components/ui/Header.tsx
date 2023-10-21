3cabd79ca39a7b5506abb39b04a285aec1f6e671
Trung Nguyen <trungngoc.tv7@gmail.com>
2023-10-21 21:28:56 +0700

    T

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,7 +1,24 @@
+import { styled } from 'styled-components'
+import { NavLink } from 'react-router-dom'
 
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <div className="flex items-center gap-[60px]">
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_B%C3%A9lo.svg"
+          alt='horse'
+          width={100}  
+        />
+        <nav>
+          <NavLink to="">LỊCH CHIẾU</NavLink>
+          <NavLink to="">PHIM</NavLink>
+          <NavLink to="">RẠP</NavLink>
+          <NavLink to="">TIN TỨC</NavLink>
+        </nav>
+      </div>
+    </div>
   )
 }
+
